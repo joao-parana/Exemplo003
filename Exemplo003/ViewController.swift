c2c0963e5b93a19e1e05186ba425a5320f7432fc
João Antonio Ferreira <joao.parana@gmal.com>
2014-09-30 13:59:48 -0300

    Initial Commit

---
+++ Exemplo003/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exemplo003
+//
+//  Created by Joao Ferreira on 9/30/14.
+//  Copyright (c) 2014 si. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
